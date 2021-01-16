d28399b41287290f153dd2fe4a0737d9527dc90c
miraj <mirajulisam7993@gmail.com>
2021-01-16 19:34:42 +0600

    configure tomcat deploye  and cross orgin use

--- Online-Mobile-Shop/src/main/java/com/example/OnlineMobileShopApplication.java
+++ Online-Mobile-Shop/src/main/java/com/example/OnlineMobileShopApplication.java
@@ -2,9 +2,16 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class OnlineMobileShopApplication {
+public class OnlineMobileShopApplication extends SpringBootServletInitializer{
+	
+	  @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(OnlineMobileShopApplication.class);
+	    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineMobileShopApplication.class, args);
--- Online-Mobile-Shop/src/main/java/com/example/controller/PhoneController.java
+++ Online-Mobile-Shop/src/main/java/com/example/controller/PhoneController.java
@@ -18,6 +18,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -40,6 +41,7 @@ import net.sf.jasperreports.engine.export.JRPdfExporterParameter;
 /**
  * @author mirajul.islam
  */
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class PhoneController {
 	private static Logger log = LoggerFactory.getLogger(PhoneController.class);
