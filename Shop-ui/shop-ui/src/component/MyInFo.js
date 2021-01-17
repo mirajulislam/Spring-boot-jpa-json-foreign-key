bdf80906bc35125d097e7d7976fdf52c7d66de80
mirajul_islam57 <mirajulisam7993@gmail.com>
2021-01-17 23:40:35 +0600

    functional components

--- Shop-ui/shop-ui/src/component/MyInFo.js
+++ Shop-ui/shop-ui/src/component/MyInFo.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Footer from './footerElement/Footer'
-function App(){
+
+function MyInFo(){
   return(
     <div>
-      <nav>
       <h1>Hello My Friend..</h1>
       <p>How are you everyone?</p>
       <ul>
@@ -13,14 +12,9 @@ function App(){
         <li>Miraj</li>
         <li>Aminul</li>
       </ul>
-      </nav>    
-      <main>
-        <p>I love my Country</p>
-      </main> 
-      <Footer />
     </div>
   )  
 }
 
 
-export default App;
+export default MyInFo;
