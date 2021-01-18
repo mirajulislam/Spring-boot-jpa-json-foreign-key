20b4f6ff183702ab83cff228410aca1cb50ae7a5
mirajul_islam57 <mirajulisam7993@gmail.com>
2021-01-18 23:24:10 +0600

    JSX to JavaScript and Back,............. Inline Styles with the Style Property.........

--- Shop-ui/shop-ui/src/App.js
+++ Shop-ui/shop-ui/src/App.js
@@ -5,18 +5,27 @@ import Header from './Element/Header';
 import MainContent from './Element/MainContent';
 
 function App(){
+  const date=new Date(2021,1,6,11)
+  const hours=date.getHours()
+  let timeDay
+
+  const styles = {
+   fontSize:30
+  }
+
+  if(hours<12){
+     timeDay    ="Good Morning"
+     styles.color = "#FF7F50"
+  }else if(hours>=12 && hours<17){
+     timeDay    ="afternoon Morning"
+     styles.color = "##40E0D0"
+  }else{
+    timeDay    ="Good Night"
+    styles.color = "#DE3163"
+  }
   return(
-    <div>
-     <input type="checkbox" />
-     <p>Placeholder text here</p>
-     <input type="checkbox" />
-     <p>Placeholder text here</p>
-     <input type="checkbox" />
-     <p>Placeholder text here</p>
-     <input type="checkbox" />
-     <p>Placeholder text here</p>
-    </div>
-  )  
+    <h1 style={styles}>{timeDay}</h1>
+  )
 }
 
 
