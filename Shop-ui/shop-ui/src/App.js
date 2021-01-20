17023b8c36e22d49c580e751a99fb2cffb36518e
mirajul_islam57 <mirajulisam7993@gmail.com>
2021-01-20 23:45:17 +0600

    class compnents

--- Shop-ui/shop-ui/src/App.js
+++ Shop-ui/shop-ui/src/App.js
@@ -1,33 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Joke from './Joke';
+import React, { Component } from 'react';
 
-function App(){
-  return(
+class App extends Component {
+  render() {
+    return (
       <div>
-        <Joke 
-          answer="What Is your name ? as as as ds " 
-        />
-        <Joke 
-          question="What Is your name ?" 
-          answer="My name is raj"
-        />
-
-        <Joke 
-          question="What Is your name ?" 
-          answer="My name is raj"
-        />
-
-        <Joke 
-          question="What Is your name ?" 
-          answer="My name is raj"
-        />
-
-        <Joke 
-          question="What Is your name ?"
-          answer="My name is raj"
-        />
+        <h1>hi</h1>
       </div>
-  )
+    );
+  }
 }
+
 export default App;
